32471c6ac0a8384ee17fa1584cd0881dabd21a63
cyberfunk <hello@cyberfunk.org>
2014-09-28 16:54:05 +0800

    Expose class constructor.

--- index.js
+++ index.js
@@ -334,6 +334,7 @@ function toObject(opts) {
   o.name = this.name();
   o.description = this.description();
   o.key = this.key();
+  o.constructor = this.constructor;
   if(opts.all || opts.names) o.names = this.names();
   if(opts.all || opts.extra) o.names = this.extra();
   if(opts.all || opts.value) o.names = this.value();
